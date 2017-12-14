c8ceec67848898e0801f4b96cf2734f966722401
Devesh Anand <ananddevesh29.devp@outlook.com>
2017-12-14 13:15:22 +0530

    initial login page design

---
+++ scripts/question.js
@@ -0,0 +1,13 @@
+//constructor for question class
+function Question(id,text,hint,point,flag){
+    this.id = id;
+    this.text = text;
+    this.hint = hint;
+    this.point = point;
+    this.flag = flag;
+}
+
+//function for checking answer
+Question.prototype.checkAnswer = function (answer){
+    return this.flag === answer;
+}
