b7fe5ecc0a268dd1caa032153907ec7ec2621959
abhinavbajpai2012 <abhinavbajpai2012@gmail.com>
2018-01-12 19:53:35 +0530

    Added complete Leaderboard

---
+++ Leaderboard/leaderboard.php
@@ -0,0 +1,11 @@
+<?php
+require_once('connect.php');
+?>
+<?php
+$queryboard = "SELECT * FROM teams ORDER BY solved DESC";
+$resultbliss=mysqli_query($link,$queryboard);
+  while ($row=mysqli_fetch_assoc($resultbliss)) {
+echo "<li><mark>". $row['team_name']."</mark><small>". $row['solved']."</small></li>";
+
+}
+?>
