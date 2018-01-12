156771ead6326812b849bdc7af463d7429401bd2
abhinavbajpai2012 <abhinavbajpai2012@gmail.com>
2018-01-12 22:50:13 +0530

    leaderboard

--- Leaderboard/leaderboard.php
+++ Leaderboard/leaderboard.php
@@ -2,10 +2,9 @@
 require_once('connect.php');
 ?>
 <?php
-$queryboard = "SELECT * FROM teams ORDER BY solved DESC";
+$queryboard = "SELECT * FROM teams ORDER BY solved DESC limit 10";
 $resultbliss=mysqli_query($link,$queryboard);
-  while ($row=mysqli_fetch_assoc($resultbliss)) {
+while ($row=mysqli_fetch_assoc($resultbliss)) {
 echo "<li><mark>". $row['team_name']."</mark><small>". $row['solved']."</small></li>";
-
 }
 ?>
